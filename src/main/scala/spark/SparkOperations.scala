914f7d3dd9199ba5f3221b9518fdc53c9417e179
logicalguess <logicalguess@gmail.com>
2016-03-13 15:16:21 -0400

    spark test

--- src/main/scala/spark/SparkOperations.scala
+++ src/main/scala/spark/SparkOperations.scala
@@ -1,16 +1,12 @@
 package spark
 
-import flow.{TransformerU, Transformer, Operation}
+import flow.{TransformerU, Operation}
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 /**
   * Created by logicalguess on 3/12/16.
   */
-
-//case class SparkOperation[A](f: SparkContext => A) {
-//  def apply(sc: SparkContext) = Operation[A] { f(sc) }
-//}
 
 case class SparkOperation[A](f: SparkContext => A)
   extends TransformerU[SparkContext, A]
