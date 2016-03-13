dec59f100479c8e000ae3c9365db90ccd76867ea
logicalguess <logicalguess@gmail.com>
2016-03-13 14:53:45 -0400

    spark test

--- src/test/scala/flow/SparkSuite.scala
+++ src/test/scala/flow/SparkSuite.scala
@@ -3,27 +3,41 @@ package flow
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.scalatest.{ShouldMatchers, WordSpec}
-import spark.{LOCAL, SparkProvider, SparkOperation}
+import spark.{RDDTransformer, LOCAL, SparkProvider, SparkOperation}
 import util.Logging
 
-import scala.util.Try
+import scala.util.{Success, Try}
 
 
 class SparkSuite extends WordSpec with ShouldMatchers with Logging {
 
   "Spark examples" should {
 
-    "letters" in new TryExecutor {
+    val sc: Operation[SparkContext] = SparkProvider("test")(LOCAL)
 
-      val rdd: SparkOperation[RDD[String]] = SparkOperation[RDD[String]] { sc =>
-        sc.makeRDD("There is nothing either good or bad, but thinking makes it so".split(' '))
-      }
+    val wordsRDD: SparkOperation[RDD[String]] = SparkOperation[RDD[String]] { sc =>
+      sc.makeRDD("There is nothing either good or bad, but thinking makes it so".split(' '))
+    }
 
-      val sc: Operation[SparkContext] = SparkProvider("test")(LOCAL)
+    val aWords = RDDTransformer[String, String] { rdd => rdd.filter(_.contains("a")) }
+    val countOperation = Transformer[RDD[String], Long] { _.count }
 
-      val operation = sc --> rdd // same as rdd(sc)
+    "words" in new TryExecutor {
+      val operation = sc --> wordsRDD // same as wordsRDD(sc)
       val result: Try[RDD[String]] = execute(operation)
       result.get.count() shouldBe 12
+    }
+
+    "letter count" in new TryExecutor {
+      val flow = for {
+        ctx <- sc
+        words <- wordsRDD(ctx)
+        aWords <- aWords(words)
+        count <- countOperation(aWords)
+      }
+        yield count
+
+      execute(flow) shouldBe Success(2)
     }
   }
 }
