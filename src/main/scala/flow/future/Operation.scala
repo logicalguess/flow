6d89fbadc0925956d064e1df61cedeffd6bb0b20
logicalguess <logicalguess@gmail.com>
2016-03-15 17:33:19 -0400

    future operations

--- src/main/scala/flow/future/Operation.scala
+++ src/main/scala/flow/future/Operation.scala
@@ -3,27 +3,12 @@ package flow.future
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-
-//import scalaz._
-
-
 sealed trait Operation[A] extends (() => Future[A]) {
-  //def apply(): A
-
-  // This enables out the box map / flatMap / for comprehension support
-  //def map[B](f: A ⇒ B): Operation[B] = Operation.monad.map(this)(f)
-  //def flatMap[B](f: A ⇒ Operation[B]): Operation[B] = Operation.monad.bind(this)(f)
 
   def map[B](f: A ⇒ B): Operation[B] = Operation(apply().map(f))
-  def flatMap[B](f: A => Operation[B]): Operation[B] = Operation(apply().flatMap( a => f(a)())) //Operation(f(apply()).apply())
+  def flatMap[B](f: A => Operation[B]): Operation[B] = Operation(apply().flatMap( a => f(a)()))
 
-  def -->[B] (t: TransformerU[A, B]): Operation[B] = {
-//    val fa: Future[A] = apply()
-//    val fun : A=>Future[B] =  {a: A => t(a)()}
-//    val r: Future[B] = fa.flatMap(fun)
-//    Operation(r)
-    Operation(apply().flatMap({a: A => t(a)()}))
-  } //t(apply())
+  def -->[B] (t: TransformerU[A, B]): Operation[B] = Operation(apply().flatMap({a: A => t(a)()}))
 }
 
 object OperationImplicits {
@@ -34,20 +19,9 @@ object OperationImplicits {
 }
 
 object Operation {
-//  def apply[A](f: => A): Operation[A] = new Operation[A] {
-//    override def apply = Future { f }
-//  }
-
   def apply[A](f: => Future[A]): Operation[A] = new Operation[A] {
     override def apply = f
   }
-
-//  implicit val monad = new Monad[Operation] {
-//    override def bind[A, B](fa: Operation[A])(f: A ⇒ Operation[B]): Operation[B] =
-//      Operation(f(fa.apply()).apply())
-//
-//    override def point[A](a: ⇒ A): Operation[A] = Operation(a)
-//  }
 }
 
 trait TransformerU[In, Out] {
