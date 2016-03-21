6685f51d54524a1513b4ad4eaf3aa65cfffaffac
logicalguess <logicalguess@gmail.com>
2016-03-20 23:50:58 -0400

    functions object

--- src/main/scala/finatra/service/FlowALSRecommenderService.scala
+++ src/main/scala/finatra/service/FlowALSRecommenderService.scala
@@ -12,6 +12,8 @@ import org.apache.spark.rdd.RDD
 
 import scala.util.Random
 
+import Functions._
+
 /**
   * Created by logicalguess on 2/26/16.
   */
@@ -24,21 +26,15 @@ case class FlowALSRecommenderService @Inject()(sc: SparkContext, dataProvider: D
   def getRecommendationsForUser(userId: Int, count: Int) = {
 
     val userIdFn = {userId}
-
     val sparkContextFn = {sc}
-
     val modelFn = {model}
-
     val productsFn = dataProvider.getProductNames
-
     val candidatesFn: ((SparkContext, Map[Int, String])) => RDD[Int] = t => t match {
       case (sc: SparkContext, products: Map[Int, String]) => sc.parallelize(products.keys.toSeq)
     }
-
     val mapByIdFn: ((Int, RDD[Int])) => RDD[(Int, Int)] =  t => t match {
       case (userId: Int, rdd: RDD[Int]) => rdd.map((userId, _))
     }
-
     val predictFn: ((MatrixFactorizationModel, RDD[(Int, Int)])) => Array[Rating] = t => t match {
       case (model,  rdd: RDD[(Int, Int)]) =>
         model
@@ -47,7 +43,6 @@ case class FlowALSRecommenderService @Inject()(sc: SparkContext, dataProvider: D
         .sortBy(- _.rating)
         .take(count)
     }
-
     //val fns = predictFn(mapByIdFn(userId, candidatesFn(sparkContextFn, productsFn)))
 
     val graph = DAG("flow",
@@ -73,77 +68,9 @@ case class FlowALSRecommenderService @Inject()(sc: SparkContext, dataProvider: D
 
   }
 
-  /** Compute RMSE (Root Mean Squared Error). */
-  def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating]) = {
-    val n = data.count
-    val predictions: RDD[Rating] = model.predict(data.map(x => (x.user, x.product)))
-    val predictionsAndRatings = predictions.map(x => ((x.user, x.product), x.rating))
-      .join(data.map(x => ((x.user, x.product), x.rating)))
-      .values
-    math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).reduce(_ + _) / n)
-  }
-
   def createModel(): (MatrixFactorizationModel, Double, String) = {
 
-
-    val numPartitions = 20
-
     val ratingsFn: RDD[Rating] = dataProvider.getRatings
-
-    def addRandomLongColumnFn(rs: RDD[Rating]): RDD[(Long, Rating)]  = {
-      val rand = new Random()
-      rs.map { r => (rand.nextInt(10).toLong, r) }
-    }
-
-    def trainingFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
-      rs.filter(x => x._1 <= 3)
-        .values
-        .repartition(numPartitions)
-        .persist
-    }
-    //val fn: (RDD[(Long, Rating)]) => RDD[Rating] = trainingFilterFn _
-
-    def validationFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
-      rs.filter(x => x._1 == 4)
-        .values
-        .repartition(numPartitions)
-        .persist
-    }
-
-    def testingFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
-      rs.filter(x => x._1 == 5)
-        .values
-        .persist
-    }
-
-    def train(training: RDD[Rating], validation: RDD[Rating]): MatrixFactorizationModel = {
-      val ranks = List(12)
-      val lambdas = List(0.1, 10.0)
-      val numIters = List(10)
-
-      var bestModel: Option[MatrixFactorizationModel] = None
-      var bestValidationRmse = Double.MaxValue
-      var bestRank = 0
-      var bestLambda = -1.0
-      var bestNumIter = -1
-
-      val numValidation = validation.count
-
-      for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
-        val model = ALS.train(training, rank, numIter, lambda)
-
-        val validationRmse = computeRmse(model, validation)
-
-        if (validationRmse < bestValidationRmse) {
-          bestModel = Some(model)
-          bestValidationRmse = validationRmse
-          bestRank = rank
-          bestLambda = lambda
-          bestNumIter = numIter
-        }
-      }
-      bestModel.get
-    }
 
 //    val ratings = addRandomLongColumnFn(ratingsFn)
 //    val model = train(trainingFilterFn(ratings), validationFilterFn(ratings))
@@ -174,5 +101,76 @@ case class FlowALSRecommenderService @Inject()(sc: SparkContext, dataProvider: D
 
     (ops("model")().asInstanceOf[MatrixFactorizationModel], ops("rmse")().asInstanceOf[Double],
       Util.gravizoDotLink(DAG.dotFormatDiagram(graph)))
+  }
+}
+
+object Functions {
+
+  val numPartitions = 20
+
+  /** Compute RMSE (Root Mean Squared Error). */
+  def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating]) = {
+    val n = data.count
+    val predictions: RDD[Rating] = model.predict(data.map(x => (x.user, x.product)))
+    val predictionsAndRatings = predictions.map(x => ((x.user, x.product), x.rating))
+      .join(data.map(x => ((x.user, x.product), x.rating)))
+      .values
+    math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).reduce(_ + _) / n)
+  }
+
+
+  def addRandomLongColumnFn(rs: RDD[Rating]): RDD[(Long, Rating)]  = {
+    val rand = new Random()
+    rs.map { r => (rand.nextInt(10).toLong, r) }
+  }
+
+  def trainingFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
+    rs.filter(x => x._1 <= 3)
+      .values
+      .repartition(numPartitions)
+      .persist
+  }
+  //val fn: (RDD[(Long, Rating)]) => RDD[Rating] = trainingFilterFn _
+
+  def validationFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
+    rs.filter(x => x._1 == 4)
+      .values
+      .repartition(numPartitions)
+      .persist
+  }
+
+  def testingFilterFn(rs: RDD[(Long, Rating)]): RDD[Rating]  = {
+    rs.filter(x => x._1 == 5)
+      .values
+      .persist
+  }
+
+  def train(training: RDD[Rating], validation: RDD[Rating]): MatrixFactorizationModel = {
+    val ranks = List(12)
+    val lambdas = List(0.1, 10.0)
+    val numIters = List(10)
+
+    var bestModel: Option[MatrixFactorizationModel] = None
+    var bestValidationRmse = Double.MaxValue
+    var bestRank = 0
+    var bestLambda = -1.0
+    var bestNumIter = -1
+
+    val numValidation = validation.count
+
+    for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
+      val model = ALS.train(training, rank, numIter, lambda)
+
+      val validationRmse = computeRmse(model, validation)
+
+      if (validationRmse < bestValidationRmse) {
+        bestModel = Some(model)
+        bestValidationRmse = validationRmse
+        bestRank = rank
+        bestLambda = lambda
+        bestNumIter = numIter
+      }
+    }
+    bestModel.get
   }
 }
--- src/main/scala/util/FunctionImplicits.scala
+++ src/main/scala/util/FunctionImplicits.scala
@@ -4,5 +4,13 @@ package util
   * Created by logicalguess on 3/19/16.
   */
 object FunctionImplicits {
-  implicit def FunctionToPartialFunction[In, Out] (f: Function[In, Out]): PartialFunction[Any, Any] = { case in: In => f(in)}
+  implicit def functionToPartialFunction[In, Out] (f: Function[In, Out]): PartialFunction[Any, Any] = { case in: In => f(in)}
 }
+
+//val f = m _
+//
+//// is equivalent to
+//
+//val f = new AnyRef with Function1[List[Int], AnyRef] {
+//  def apply(x$1: List[Int]) = this.m(x$1)
+//}
