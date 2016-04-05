8db652abe7993e43bc0b4f9d6a2373e10beb8c4e
logicalguess <logicalguess@gmail.com>
2016-04-05 11:12:17 -0400

    gearpump app

---
+++ src/main/scala/flow/gearpump/PipelineApp.scala
@@ -0,0 +1,65 @@
+package flow.gearpump
+
+/**
+  * Created by logicalguess on 4/4/16.
+  */
+
+import akka.actor.ActorSystem
+import com.typesafe.config.{Config, ConfigFactory}
+import dag.DAG
+import io.gearpump.cluster.UserConfig
+import io.gearpump.cluster.client.ClientContext
+import io.gearpump.cluster.main.{ArgumentsParser, CLIOption, ParseResult}
+import io.gearpump.streaming.StreamApplication
+import io.gearpump.util.Graph._
+import io.gearpump.util.{AkkaApp, Graph, LogUtil}
+import org.slf4j.Logger
+
+import util.FunctionImplicits._
+
+
+case class PipelineConfig(config: Config) extends java.io.Serializable
+
+
+object PipelineApp extends AkkaApp with ArgumentsParser {
+  private val LOG: Logger = LogUtil.getLogger(getClass)
+
+  override val options: Array[(String, CLIOption[Any])] = Array(
+    //"processors"-> CLIOption[Int]("<processor number>", required = false, defaultValue = Some(1)),
+   )
+
+  def application(config: ParseResult, system: ActorSystem): StreamApplication = {
+    implicit val actorSystem = system
+    val pipelineString =
+      """
+        |pipeline {
+        |  processors = 1
+        |}
+      """.stripMargin
+
+    val pipelineConfig = PipelineConfig(ConfigFactory.parseString(pipelineString))
+
+    val appConfig = UserConfig.empty.withValue[PipelineConfig]("Pipeline", pipelineConfig)
+
+    val graph = DAG("flow",
+      List("second"), List("third", "second"), List("fourth", "third"))
+
+    val f_str = { i: Int => i.toString }
+    val f_bang = {  s: String =>  s + "!" }
+    val f_hash = { s: String =>  s + "#" }
+
+    val functions: Map[String, Function[Any, Any]] = Map(
+      "second" -> f_str, "third" -> f_bang,
+      "fourth" -> f_hash)
+
+    GearStreamingApp(graph, functions, appConfig)
+  }
+
+  override def main(akkaConf: Config, args: Array[String]): Unit = {
+    val config = parse(args)
+    val context = ClientContext(akkaConf)
+    val appId = context.submit(application(config, context.system))
+    context.close()
+  }
+
+}
