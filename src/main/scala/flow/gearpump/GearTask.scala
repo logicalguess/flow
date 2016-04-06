e83a13b3607d2ca888869efc11afa8f4b341aff8
logicalguess <logicalguess@gmail.com>
2016-04-06 16:21:34 -0400

    root start

--- src/main/scala/flow/gearpump/GearTask.scala
+++ src/main/scala/flow/gearpump/GearTask.scala
@@ -11,10 +11,11 @@ import io.gearpump.streaming.task.{StartTime, Task, TaskContext}
 import scala.util.{Failure, Success, Try}
 
 class GearTask(taskContext : TaskContext, config: UserConfig) extends Task(taskContext, config) {
+  val START = null
   val fun: Function[Any, Any] = config.getValue[Function[Any, Any]]("function").get
 
   override def onStart(startTime: StartTime): Unit = {
-    if (config.getBoolean("root").get) self ! Message("start", System.currentTimeMillis)
+    if (config.getBoolean("root").get) self ! Message(START, System.currentTimeMillis)
   }
 
   override def onNext(msg: Message): Unit = {
